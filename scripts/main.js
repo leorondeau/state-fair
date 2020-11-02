06da43a90a7907776458bc836e8a764eb67b5ed1
admin <christopherleorondeau@gmail.com>
2020-11-01 22:17:48 -0600

    invoked SideShowHolders function in main.js to test functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 import { FoodTicketHolders } from "./food/FoodTicketHolder.js"
+import { GameTicketHolders } from "./games/GameTicketHolder.js"
 import { RideTicketHolders } from "./rides/RideTicketHolders.js"
+import { SideShowTicketHolders } from "./sideshows/SideShowHolder.js"
 import { TicketBooth } from "./TicketBooth.js"
 
 // Import and invoke the ticket booth component function
 TicketBooth()
 RideTicketHolders()
 FoodTicketHolders()
+GameTicketHolders()
+SideShowTicketHolders()
